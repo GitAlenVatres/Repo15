74584d682e906d3487374b610317f9c295a34820
GitAlenVatres <90286976+GitAlenVatres@users.noreply.github.com>
2021-09-28 09:35:14 +0200

    Add files via upload

---
+++ ex08/src/index.js
@@ -0,0 +1,10 @@
+//Only change code below this line
+const sum = (num1,num2) => {
+num1 =4;
+num2 =6;
+};
+//Only change code above this line
+
+
+console.log(sum(num1,num2)); //Change this line
+module.exports = sum;
