71bcf4fd215dccb74ad1d470ad7a9c1d181dfe9f
GitAlenVatres <90286976+GitAlenVatres@users.noreply.github.com>
2021-09-28 09:20:47 +0200

    Add files via upload

---
+++ ex03/src/index.js
@@ -0,0 +1,14 @@
+function printManyTimes(str) {
+"use strict";
+
+//Only change code below this line
+var sentence = str + " is cool!";
+for (var i =0; i<str.length;i+=2) {
+console.log (sentence);
+}
+
+return sentence;
+//only change code above this line
+}
+printManyTimes("Arena");
+module.exports = printManyTimes;
