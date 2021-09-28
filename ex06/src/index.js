28b9696df9cc927e1dc19139ae312f64c758be00
GitAlenVatres <90286976+GitAlenVatres@users.noreply.github.com>
2021-09-28 09:28:46 +0200

    Add files via upload

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+//Only change code below this line
+const arena =function () {
+const new Date = ();
+//Only change code above this line
+"use strict";
+return new Date();
+};
+
+console.log(arena());
+module.exports = arena;
